b4841e00bfcc7632b89f152d49d4ba140c1f531b
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2022-05-06 13:36:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from datetime import datetime
 import boto3
 import nltk
 import json
-import pyaudio
-from contextlib import closing
+import wave
 
 translate = boto3.client('translate')
 polly = boto3.client('polly')
@@ -16,7 +15,6 @@ currentDate = datetime.now()
 totalNews = {'English':{}, 'Hindi': {}}
 speakText = ""
 speakTextHindi = ""
-stop = False
 
 app = Flask(__name__)
 @app.route('/')
@@ -61,66 +59,35 @@ def getNews():
         if lastUpdate==24:
             lastUpdate = 0
         print(lastUpdate)
+        SAMPLE_RATE = 16000
+        CHANNELS = 1
+        WAV_SAMPLE_WIDTH_BYTES = 2
+        OUTPUT_FILE_IN_WAVE = 'static/englishSpeech.wav'
+        FRAMES = []
+        response=polly.synthesize_speech(Text=speakText[:1000],VoiceId='Aditi',OutputFormat='pcm',SampleRate=str(SAMPLE_RATE))
+        stream = response.get("AudioStream")
+        FRAMES.append(stream.read())
+        WAVEFORMAT = wave.open(OUTPUT_FILE_IN_WAVE,'wb')
+        WAVEFORMAT.setnchannels(CHANNELS)
+        WAVEFORMAT.setsampwidth(WAV_SAMPLE_WIDTH_BYTES)
+        WAVEFORMAT.setframerate(SAMPLE_RATE)
+        WAVEFORMAT.writeframes(b''.join(FRAMES))
+        WAVEFORMAT.close()
+        OUTPUT_FILE_IN_WAVE = 'static/hindiSpeech.wav'
+        FRAMES = []
+        response2=polly.synthesize_speech(Text=speakTextHindi[:1000],VoiceId='Aditi',OutputFormat='pcm',SampleRate=str(SAMPLE_RATE))
+        stream2 = response2.get("AudioStream")
+        FRAMES.append(stream2.read())
+        WAVEFORMAT2 = wave.open(OUTPUT_FILE_IN_WAVE,'wb')
+        WAVEFORMAT2.setnchannels(CHANNELS)
+        WAVEFORMAT2.setsampwidth(WAV_SAMPLE_WIDTH_BYTES)
+        WAVEFORMAT2.setframerate(SAMPLE_RATE)
+        WAVEFORMAT2.writeframes(b''.join(FRAMES))
+        WAVEFORMAT2.close()
         return json.dumps(totalNews)
 def hindi_translate(i):
     result=translate.translate_text(Text=i,SourceLanguageCode='en',TargetLanguageCode='hi')
     return result['TranslatedText']
-
-@app.route('/speak')
-def speak():
-    global speakText
-    global stop
-    SAMPLE_RATE = 16000
-    READ_CHUNK = 4096
-    CHANNELS = 1
-    BYTES_PER_SAMPLE = 2
-    response=polly.synthesize_speech(Text=speakText[:1000],VoiceId='Aditi',OutputFormat='pcm',SampleRate=str(SAMPLE_RATE))
-    p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(BYTES_PER_SAMPLE),channels=CHANNELS,rate=SAMPLE_RATE,output=True)
-    with closing(response["AudioStream"]) as polly_stream:
-        while True:
-            data = polly_stream.read(READ_CHUNK)
-            if data is None:
-                break
-            if stop:
-                stop = False
-                break
-            stream.write(data)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    return 'Success'
-
-@app.route('/speakHindi')
-def speakHindi():
-    global speakTextHindi
-    global stop
-    SAMPLE_RATE = 16000
-    READ_CHUNK = 4096
-    CHANNELS = 1
-    BYTES_PER_SAMPLE = 2
-    response=polly.synthesize_speech(Text=speakTextHindi[:1000],VoiceId='Aditi',OutputFormat='pcm',SampleRate=str(SAMPLE_RATE))
-    p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(BYTES_PER_SAMPLE),channels=CHANNELS,rate=SAMPLE_RATE,output=True)
-    with closing(response["AudioStream"]) as polly_stream:
-        while True:
-            data = polly_stream.read(READ_CHUNK)
-            if data is None:
-                break
-            if stop:
-                stop = False
-                break
-            stream.write(data)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    return 'Success'
-
-@app.route('/stopSpeech')
-def stopSpeech():
-    global stop
-    stop = True
-    return 'Success'
 
 if __name__=='__main__':
     app.run(host='localhost')
